fe145fdc0792bbfed3d834aabb738241663be4e7
L3gacyyy <strktrrr@proton.me>
2024-06-21 16:21:42 +0200

    Update and rename test.js to iosversion.js

--- iosversion.js
+++ iosversion.js
@@ -1,6 +1,3 @@
-/**
- * Function to detect the visitor's iOS version and display a message.
- */
 function detectIOSVersion() {
     const iOSVersion = parseFloat(
         ('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0, ''])[1])
@@ -9,7 +6,6 @@ function detectIOSVersion() {
 
     let message = "You are using iOS version " + iOSVersion;
     
-    // Creating a message element to display the iOS version
     const messageElement = document.createElement('div');
     messageElement.textContent = message;
     messageElement.style.backgroundColor = '#f0f0f0';
@@ -19,10 +15,8 @@ function detectIOSVersion() {
     messageElement.style.bottom = '10px';
     messageElement.style.left = '10px';
     messageElement.style.border = '1px solid #ccc';
-    
-    // Appending the message element to the body of the webpage
+  
     document.body.appendChild(messageElement);
 }
 
-// Call the detectIOSVersion function when the page loads
 window.onload = detectIOSVersion;
