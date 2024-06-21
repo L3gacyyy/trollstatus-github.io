00fd64f3d9fcef68d480e8b508741c999acce927
L3gacyyy <strktrrr@proton.me>
2024-06-21 17:07:39 +0200

    Update iosversion.js

--- iosversion.js
+++ iosversion.js
@@ -4,6 +4,22 @@ function detectIOSVersion() {
             .replace('undefined', '3_2').replace('_', '.').replace('_', '')
     );
 
+    	function displayJpgPicture(userAgent) {
+    // Check if the user agent indicates iOS and the version is between 1 and 16.5.1
+    if (/iP(hone|od|ad)/.test(userAgent) && /OS\s([\_0-9]+)/.test(userAgent)) {
+        let version = userAgent.match(/OS\s([\_0-9]+)/)[1].replace(/_/g, '.');
+        let versionNumber = parseFloat(version);
+ 
+        if (versionNumber >= 1 && versionNumber <= 16.5) {
+            // Display the jpg picture
+            console.log("Displaying jpg picture for iOS version 1 to 16.5.1");
+        } else {
+            console.log("iOS version is not within the specified range.");
+        }
+    } else {
+        console.log("Visitor is not using an iOS device.");
+    };
+
     let message = "You are using iOS version " + iOSVersion;
     
     const messageElement = document.createElement('div');
